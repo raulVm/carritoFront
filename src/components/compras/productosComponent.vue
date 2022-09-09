01c8b5756ea227427a7a11a258efb1561197c7e0
RaulVM <daul_94@hotmail.com>
2022-09-09 13:50:49 -0500

    creación del proyecto y componente de producto

---
+++ src/components/compras/productosComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>super</h2>
+</template>
+<script>
+    import{ defineComponent } from 'vue';
+    export default defineComponent({
+        name: 'productosComponent',
+        data () {
+            
+        }
+    })
+</script>
